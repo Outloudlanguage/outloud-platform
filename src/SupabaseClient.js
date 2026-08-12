c1150e70d49308868e7f068a4c3064fd7c574faa
Outloudlanguage <outloudlanguage@gmail.com>
2026-08-12 17:46:19 -0400

    Nuclear Supabase fix

--- src/SupabaseClient.js
+++ src/SupabaseClient.js
@@ -1,13 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 
-  process.env.REACT_APP_SUPABASE_URL || 
-  process.env.VITE_SUPABASE_URL || 
-  'https://kuvsmrheywhzxfiyivtg.supabase.co';
-
-const supabaseAnonKey = 
-  process.env.REACT_APP_SUPABASE_ANON_KEY || 
-  process.env.VITE_SUPABASE_ANON_KEY || 
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1dnNtcmhleXdoenhmaXlpdnRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYyMTc4MzYsImV4cCI6MjEwMTc5MzgzNn0.upJqo4zdmO3xj4KN7zUURDTI0ZY2RNWqgvLbSSCu3BA';
+const supabaseUrl = 'https://kuvsmrheywhzxfiyivtg.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1dnNtcmhleXdoenhmaXlpdnRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYyMTc4MzYsImV4cCI6MjEwMTc5MzgzNn0.upJqo4zdmO3xj4KN7zUURDTI0ZY2RNWqgvLbSSCu3BA';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
