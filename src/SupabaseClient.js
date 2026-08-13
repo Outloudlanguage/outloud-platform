a7c3c267627967573ce22143e11295dcfda9398e
Outloudlanguage <outloudlanguage@gmail.com>
2026-08-13 15:37:23 -0400

    Add Student Registration Engine

--- src/SupabaseClient.js
+++ src/SupabaseClient.js
@@ -1,5 +1,5 @@
+import { supabase } from './SupabaseClient'; 
 import { createClient } from '@supabase/supabase-js';
-
 const supabaseUrl = 'https://kuvsmrheywhzxfiyivtg.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1dnNtcmhleXdoenhmaXlpdnRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYyMTc4MzYsImV4cCI6MjEwMTc5MzgzNn0.upJqo4zdmO3xj4KN7zUURDTI0ZY2RNWqgvLbSSCu3BA';
 
